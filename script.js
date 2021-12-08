49603adaa25aae8359917112ce3772610d56e679
unknown <milkessagebi@gmail.com>
2021-12-08 22:44:24 +0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 "use strict";
 
-const model = document.querySelector(".modal");
+const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModel = document.querySelector(".close-modal");
 const btns = document.querySelectorAll(".show-modal");
 console.log(btns);
 
-for (let i = 0; i < btns.length; i++) {
-  console.log(btns[i].textContent);
-}
+for (let i = 0; i < btns.length; i++)
+  btns[i].addEventListener("click", function () {
+    console.log("Button Clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+const closeModel = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+btnCloseModel.addEventListener("click", closeModel());
+overlay.addEventListener("click", closeModel());
