efb00d7194ac584fa35710a2369d48ea652f3ef0
unknown <milkessagebi@gmail.com>
2021-12-08 20:29:00 +0300

    .

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("Hello");
+
+const model = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModel = document.querySelector(".close-modal");
+const btns = document.querySelectorAll(".show-modal");
+console.log(btns);
+
+for (let i = 0; i < btns.length; i++) {
+  console.log(btns[i].textContent);
+}
